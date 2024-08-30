a97d614900806e1a94c8f3239480fca97367608d
Rumaisa Azeem <rumaisa.azeem@hpe.com>
2024-08-30 13:43:55 +0100

    initial commit

---
+++ game.py
@@ -0,0 +1,34 @@
+import pygame
+from pygame.locals import *
+from pygame import mixer
+import pickle
+from os import path
+
+
+pygame.mixer.pre_init(44100, -16, 2, 512)
+mixer.init()
+pygame.init()
+
+# window stuff
+
+clock = pygame.time.Clock()
+fps = 60
+
+screen_width = 800
+screen_height = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Platformer')
+
+# window background
+
+bg_img = pygame.image.load('img/sky.png')
+
+
+
+run = True
+while (run): # this keeps the window up for now, replace with main game loop eventually
+    clock.tick(fps)
+    
+    screen.blit(bg_img, (0,0))
+    pygame.display.update()
